023698ed6f50aa2f33c299749d61e2bf420edd07
Abdul Halim <abdul.halim@contracted.sampoerna.com>
2024-07-20 11:13:51 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+// app/Http/Controllers/AuthController.php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Passport\Passport;
+
+class AuthController extends Controller
+{
+
+    /**
+     * Register a new user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('Personal Access Token')->accessToken;
+       
+
+        return response()->json(['token' => $token], 201);
+    }
+
+    /**
+     * Login a user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('Personal Access Token')->accessToken;
+
+        return response()->json(['token' => $token]);
+    }
+}
